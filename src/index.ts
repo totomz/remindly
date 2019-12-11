c424333ab2361e3f6716fc8b9e233dde98e6dab1
tommaso.doninelli <tommaso.doninelli@my-ideas.it>
2019-12-11 09:49:51 +0100

    Do not send any reminder if there are no tasks to do :)

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,12 @@ import container from "./inversify.config";
 import { Trello } from "./lib/Trello";
 import TYPES from "./TYPES";
 import { Logger } from "./lib/Logger";
-import { SES } from 'aws-sdk';
+import { AWSError, SES } from 'aws-sdk';
 import { Config } from "./Config";
 import { SendEmailRequest } from "aws-sdk/clients/ses";
 
 
-
-
-export function awslambda(event, context, callback) {
+export async function awslambda(event, context): Promise<any> {
 
     const log = container.get<Logger>(TYPES.Logger);
     const config = container.get<Config>(TYPES.Configuration);
@@ -20,33 +18,42 @@ export function awslambda(event, context, callback) {
 
     const ses = new SES();
 
-    trello.getCards().then(cards => {
-        let tasks = cards.map(c => {
-            return `<li>${c.name}</li>`;
-        }).join('');
-        tasks = `<ul>${tasks}</ul>`;
+    return trello
+        .getCards()
+        .then(cards => {
 
-        return ses.sendEmail({
-            Destination: { ToAddresses: ['tommaso.doninelli@gmail.com'] },
-            Source: 'remindly@my-ideas.it',
-            Message: {
-                Subject: {
-                    Charset: "UTF-8",
-                    Data: "Remindly"
-                },
-                Body: {
-                    Html: {
-                        Charset: "UTF-8",
-                        Data: `<p>Hey man! You have stuff to do!</p>${tasks}`
-                    }
-                }
+            if (cards.length === 0) {
+                return "No cards, no reminder :)";
             }
 
-        } as SendEmailRequest).promise();
-    });
 
-        
+            let tasks = cards.map(c => {
+                return `<li>${c.name}</li>`;
+            }).join('');
+            tasks = `<ul>${tasks}</ul>`;
+
+            return ses.sendEmail({
+                Destination: {ToAddresses: ['tommaso.doninelli@gmail.com']},
+                Source: 'remindly@my-ideas.it',
+                Message: {
+                    Subject: {
+                        Charset: "UTF-8",
+                        Data: "Remindly"
+                    },
+                    Body: {
+                        Html: {
+                            Charset: "UTF-8",
+                            Data: `<p>Hey man! You have stuff to do!</p>${tasks}`
+                        }
+                    }
+                }
+            }).promise().then(res => {
+                return `Message sent: ${res.MessageId}`;
+            });
+        });
 }
 
 
-// awslambda(undefined, undefined, undefined);
+awslambda(undefined, undefined).then(c => {
+    console.log(c);
+});
